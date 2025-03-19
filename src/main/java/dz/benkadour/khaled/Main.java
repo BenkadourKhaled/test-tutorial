86ebcfef8b7aa9fd5432a0f72ae99dbf6009a79a
benkadourk <benkadourk@cnas.dz>
2025-03-19 14:50:26 +0100

    the first lesson in Testing

---
+++ src/main/java/dz/benkadour/khaled/Main.java
@@ -0,0 +1,7 @@
+package dz.benkadour.khaled;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * Create By ${} on 31/10/2024
+ *
+ * @Author : KHALED_BENKADOUR
+ * @Email : BENKKADOUR.KHALED@GMAIL.COM
+ * @DATE : (19/03/2025)
+ * @TIME : (13:41)
+ * @Project_Name : TESTING-LEARNING
+ *
+ */
+public class DemoTest {
+
+    @Test
+     public void testDemo() {
+        System.out.println("Demo Testing") ;
+    }
+
+}
